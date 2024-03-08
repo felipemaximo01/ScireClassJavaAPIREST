483941bc8faf1ebba3faaeff38c61cbc9efb0d43
Felipe Maximo <felipecontato2001@hotmail.com>
2024-03-08 20:55:57 -0300

    Projeto Base

---
+++ src/main/java/com/fatec/scireclass/ScireclassApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.scireclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScireclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScireclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/scireclass/ScireclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.scireclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScireclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
